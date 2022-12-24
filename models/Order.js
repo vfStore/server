231107aad1ca9139045dc3fbfbaf737f3c67f40f
shhady <shhadyse@gmail.com>
2022-12-24 23:51:46 +0200

    order

--- models/Order.js
+++ models/Order.js
@@ -1,11 +1,24 @@
 import mongoose from "mongoose";
 
-const orderSchema = new mongoose.Schema({
-  order: { type: Array },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  address: { type: String, required: true },
-  phone: { type: String, required: true },
-});
+const orderSchema = new mongoose.Schema(
+  {
+    order: { type: Array },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    address: { type: String, required: true },
+    phone: { type: String, required: true },
+    timestamp: {
+      type: Date,
+      default: () => {
+        const twoHoursAhead = new Date();
+        twoHoursAhead.setHours(twoHoursAhead.getHours() + 2);
+        return twoHoursAhead;
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("Order", orderSchema);
